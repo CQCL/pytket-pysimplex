9da615f115fcbec47302f28226804287af260dcf
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2024-01-17 10:50:44 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ setup(
     classifiers=[
         "Environment :: Console",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
